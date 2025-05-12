83b75e5e8b444c47362365e1e989fd5453b064a5
zen0-s4ma <nohuddlelol@gmail.com>
2025-05-12 19:47:56 +0200

    update autologin

--- setup_scripts/init_terminal.sh
+++ setup_scripts/init_terminal.sh
@@ -7,7 +7,7 @@ echo "==> Creando fichero de lanzador de terminal…"
 
 USER_HOME=$(eval echo "~$USER_NAME")
 
-cp -f ./setup_scripts/autostart_terminal.desktop $USER_HOME/.config/autostart/auto-terminal.desktop
+cp -f /opt/pve-setup/autostart_terminal.desktop $USER_HOME/.config/autostart/auto-terminal.desktop
 
 echo
 echo "✅ La terminal se ejecutará automáticamente al iniciar el entorno gráfico"
