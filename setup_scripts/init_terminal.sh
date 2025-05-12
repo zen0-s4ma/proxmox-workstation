16007665cae8d6dde1a5f5db3656834162fec761
zen0-s4ma <nohuddlelol@gmail.com>
2025-05-12 20:22:34 +0200

    Update init_terminal.sh

--- setup_scripts/init_terminal.sh
+++ setup_scripts/init_terminal.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
+set -e
+
+USER_NAME="zenosama"
 
 mkdir -p ~/.config/autostart
 
 echo
 echo "==> Creando fichero de lanzador de terminal…"
 
-USER_HOME=$(eval echo "~$USER_NAME")
+USER_HOME=$(eval echo "$USER_NAME")
 
 cp -f /opt/pve-setup/autostart_terminal.desktop $USER_HOME/.config/autostart/auto-terminal.desktop
 
 echo
-echo "✅ La terminal se ejecutará automáticamente al iniciar el entorno gráfico"
+echo "==> La terminal se ejecutará automáticamente al iniciar el entorno gráfico"
