235e0243e70069b6c66de3fa09d210a6b47e1c28
zen0-s4ma <nohuddlelol@gmail.com>
2025-05-13 00:42:16 +0200

    Update setup-pve-workstation-phase5.sh

--- setup_scripts/setup-pve-workstation-phase5.sh
+++ setup_scripts/setup-pve-workstation-phase5.sh
@@ -4,9 +4,12 @@ set -e
 USER_NAME="zenosama"
 HOME="/home/$USER_NAME"
 
+echo
+echo "==> Usuario: ${USER_NAME}"
+echo "==> Home_path: ${HOME}"
 
 ##############################################################################
-# 1) Resumen del sistema con neofetch
+# 1) Seleccion de terminal
 ###############################################################################
 echo
 echo "==> Iniciando Script Fase 5 - FINAL…"
@@ -17,48 +20,60 @@ sudo update-alternatives --config x-terminal-emulator
 echo
 echo "==> ¡Sistema Proxmox Workstation configurado y listo!"
 
-##############################################################################
-# 99.b) Reinicio
-###############################################################################
-echo
-echo "==> Configuración inicial completa. Reiniciando con el entorno grafico activado..."
-echo "==> Directorio home: ${USER_HOME}"
 
+##############################################################################
+# 2) Limpieza de .bash_profile
+###############################################################################
 echo
 echo "==> Vaciando el fichero .bash_profile..."
 : > "$HOME/.bash_profile"
 
+##############################################################################
+# 3) Lanzador de terminal
+###############################################################################
 echo
-mkdir -p "${USER_HOME}/.config/autostart"
+mkdir -p "${HOME}/.config/autostart"
 echo
 echo "==> Creando fichero de lanzador de terminal…"
-cp -f /opt/pve-setup/autostart_terminal.desktop "${USER_HOME}/.config/autostart/auto_terminal.desktop"
+cp -f /opt/pve-setup/autostart_terminal.desktop "${HOME}/.config/autostart/auto_terminal.desktop"
 # Asegúrate de que el usuario tenga permisos sobre el fichero
-chown "${USER_NAME}:${USER_NAME}" "${USER_HOME}/.config/autostart/auto_terminal.desktop"
+chown "${USER_NAME}:${USER_NAME}" "${HOME}/.config/autostart/auto_terminal.desktop"
 echo
 echo "La terminal se ejecutará automáticamente al iniciar el entorno gráfico."
 echo
 
+##############################################################################
+# 4) Activar entorno grafico
+###############################################################################
 echo
 echo "==> Activando entorno grafico..."
 #sudo systemctl set-default multi-user.target
 sudo systemctl set-default graphical.target 
 
-
+##############################################################################
+# 5) Borrado del servicio de autologin
+###############################################################################
 echo
 echo "==> Borrando el servicio de autologin..."
 #Borrado del servicio de autologin
 sudo rm /etc/systemd/system/getty@tty1.service.d/override.conf
 sudo systemctl daemon-reload
 
+##############################################################################
+# 6) Cambiando a ZSH como shell por defecto y cargando el .zshrc
+###############################################################################
 echo
 echo "==> Cambiando a ZSH..."
 chsh -s /bin/zsh "$USER_NAME"
-
 echo
 zsh
 source ~/.zshrc
 
+##############################################################################
+# 99.b) Reinicio
+###############################################################################
+echo
+echo "==> Configuración inicial completa. Reiniciando con el entorno grafico activado..."
 echo "...FIN DE FASE FINAL - PULSA CUALQUIER TECLA PARA CONTINUAR. ULTIMO REBOOT Y ARRANCA CON ENTORNO GRAFICO..."
 read -n 1 -s
 sudo reboot
