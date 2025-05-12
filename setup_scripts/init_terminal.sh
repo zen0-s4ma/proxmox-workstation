356c1e618965b8bef5873b20dc14ecae6886c66f
zen0-s4ma <nohuddlelol@gmail.com>
2025-05-12 18:34:27 +0200

    update autologin y .desktop

--- setup_scripts/init_terminal.sh
+++ setup_scripts/init_terminal.sh
@@ -5,13 +5,9 @@ mkdir -p ~/.config/autostart
 echo
 echo "==> Creando fichero de lanzador de terminal…"
 
-cat > ~/.config/autostart/auto-neofetch.desktop << EOF
-[Desktop Entry]
-Type=Application
-Name=Auto Neofetch
-Exec=xfce4-terminal --command="neofetch; exec bash"
-Terminal=false
-EOF
+USER_HOME=$(eval echo "~$USER_NAME")
+
+cp -f ./setup_scripts/autostart_terminal.desktop $USER_HOME/.config/autostart/auto-terminal.desktop
 
 echo
-echo "✅ Neofetch se ejecutará automáticamente al iniciar el entorno gráfico usando xfce4-terminal."
+echo "✅ La terminal se ejecutará automáticamente al iniciar el entorno gráfico"
