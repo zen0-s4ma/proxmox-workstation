1e317b99fd966f0a4b2c0204fd10a779c89f08bc
zen0-s4ma <nohuddlelol@gmail.com>
2025-05-14 05:03:16 +0200

    Update system_update.sh

--- setup_scripts/system_update.sh
+++ setup_scripts/system_update.sh
@@ -1,27 +1,22 @@
 #!/bin/bash
 set -e
 
-echo
 echo "==> Ejecutando: update…"
 sudo apt update
 
 echo
-/usr/local/bin/system_update.sh
 echo "==> Ejecutando: upgrade…"
 sudo apt upgrade -y
 
 echo
-/usr/local/bin/system_update.sh
 echo "==> Ejecutando: full-upgrade…"
 sudo apt full-upgrade -y
 
 echo
-/usr/local/bin/system_update.sh
 echo "==> Ejecutando: autoremove…"
 sudo apt autoremove -y
 
 echo
-/usr/local/bin/system_update.sh
 echo "==> Ejecutando: autoclean…"
 sudo apt autoclean -y
 
