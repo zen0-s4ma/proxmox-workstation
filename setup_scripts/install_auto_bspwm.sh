1ef10103cba12eea3c650c648a8986a7d2cc3ab3
zen0-s4ma <nohuddlelol@gmail.com>
2025-05-24 03:24:51 +0200

    Update install_auto_bspwm.sh

--- setup_scripts/install_auto_bspwm.sh
+++ setup_scripts/install_auto_bspwm.sh
@@ -4,7 +4,8 @@ set -e
 echo
 echo "==> Instalando AutoBSPWM..."
 echo
-git clone https://github.com/r1vs3c/auto-bspwm.git ~/auto-bspwm
+#git clone https://github.com/r1vs3c/auto-bspwm.git ~/auto-bspwm
+git clone https://github.com/zen0-s4ma/auto-bspwm.git ~/auto-bspwm
 cd auto-bspwm
 chmod +x setup.sh
 ./setup.sh
